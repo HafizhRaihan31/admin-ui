e8ab87bc53b6df28dc50dea9f46dfa526c8acc9b
Hafizh Raihan <hafizhr205@gmail.com>
2026-05-13 13:02:13 +0700

    fix penamaan error pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import SignInPage from "./pages/SignIn";
 import SignUpPage from "./pages/SignUp";
-import ErrorPage from "./pages/Error";
+import ErrorPage from "./pages/error";
 import {
   createBrowserRouter,
   RouterProvider,
